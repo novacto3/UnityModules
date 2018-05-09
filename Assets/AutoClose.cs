e5c5ed65b26c59953a7ea2e4acc53408b53c5838
Alex Marcolina <amarcolina@gmail.com>
2018-05-08 17:14:12 -0700

    Auto-close the visualizer

---
+++ Assets/AutoClose.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoClose : MonoBehaviour {
+
+	void Update () {
+		if(Time.frameCount > 40) {
+      Application.Quit();
+    }
+	}
+}
