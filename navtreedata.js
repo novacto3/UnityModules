cb268092738fe60b533b2f2284511d021d6de6ce
nickjbenson <nickjbenson@gmail.com>
2020-09-21 23:19:40 +0000

    Deploying to gh-pages from  @ dcaa83f89b6df6323c430e53b3be16426acc3be1 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -45,48 +45,17 @@ var NAVTREE =
       [ "Step 3: RiggedHand and RiggedFingers Are Set Up Automatically", "hands-module.html#hands-auto-rig-step-3", null ]
     ] ],
     [ "Upgrading Unity Modules", "upgrading-unity-modules.html", [
-      [ "Special upgrade: Old XR Rigs (before Core 4.5.0)", "upgrading-unity-modules.html#upgrades-prior-to-4-4-0", null ],
+      [ "Special upgrade: Old XR Rigs (before Core 4.4.0)", "upgrading-unity-modules.html#upgrades-prior-to-4-4-0", null ],
       [ "Special upgrade: Files (before June 1st, 2017)", "upgrading-unity-modules.html#upgrades-prior-to-2017-06-01", null ]
     ] ],
     [ "Deprecated Modules", "deprecated-modules.html", null ],
-    [ "Developer Resources", "developer-resources.html", null ],
-    [ "Namespaces", "namespaces.html", "namespaces" ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ]
-    ] ]
+    [ "Developer Resources", "developer-resources.html", null ]
   ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html",
-"class_leap_1_1_frame.html#ab71e6ee46f0c2593266f9a62d9c5e029",
-"class_leap_1_1_unity_1_1_animation_1_1_internal_1_1_color_interpolator_base.html#ab81520f2529212f1308057a3b5671f09",
-"class_leap_1_1_unity_1_1_attributes_1_1_curve_bounds_attribute.html#a29e234824eaae1b76420d5ca8d0bbebd",
-"class_leap_1_1_unity_1_1_delta_buffer.html#a69fc35df72918b1fad795f6cfee92908",
-"class_leap_1_1_unity_1_1_examples_1_1_transform_handle_editor.html#a9292d21427a3a3c9c6727380f50ec25a",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_custom_float_channel_feature.html#a55c1420c7c454176eb64c0f0681b5c43",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_leap_box_graphic.html#aef8c06d324f277251bf17113b298cf41",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_leap_graphic_renderer.html#a7e6f78ef7fec2ff93665642906e45241",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_leap_panel_graphic.html#a01f8b49fa7eda37f8ddc5cb45a0a4231",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_leap_sliced_graphic.html#aaf15ae21166441602c54cfbcb2646a7c",
-"class_leap_1_1_unity_1_1_graphical_renderer_1_1_leap_texture_feature_drawer.html#a86eb5925231ee42d7cda36bd88efea00",
-"class_leap_1_1_unity_1_1_interaction_1_1_anchorable_behaviour.html#a8633d13a01ffabbeba3a44064c6b0244",
-"class_leap_1_1_unity_1_1_interaction_1_1_interaction_behaviour_editor.html#a9292d21427a3a3c9c6727380f50ec25a",
-"class_leap_1_1_unity_1_1_interaction_1_1_interaction_controller.html#a74ab3b924b330239f9327b41185310b8",
-"class_leap_1_1_unity_1_1_interaction_1_1_interaction_slider.html#a083d9ef67e2e39a742673fd0bbe6ab1e",
-"class_leap_1_1_unity_1_1_interaction_1_1_interaction_toggle.html#a2628ea8d12e8b2563c32f05dc7fff6faa50f032d1f198110050aed4826c0c9259",
-"class_leap_1_1_unity_1_1_interaction_1_1_interaction_x_r_controller.html#ad4d8c442e95bd2a0c87558c125d411f0",
-"class_leap_1_1_unity_1_1_leap_service_provider_editor.html#a486ec801e2e0d19168998c13de2cb521",
-"class_leap_1_1_unity_1_1_pinch_detector.html#a573a46c80c90954cf59286c81428bec9",
-"class_leap_1_1_unity_1_1_rigged_hand.html#a8050ecea18e5683ec9816b2dbc795179",
-"class_leap_1_1_unity_1_1_skeletal_hand.html#ab5c5634050c278b877480f1821c66444",
-"class_leap_1_1_unity_1_1_tests_1_1_hand_factory_right.html#a0cab5466a9d9c96403931eead74305a3",
-"class_leap_internal_1_1_connection.html#a2faca2fee18184d9f5d52aead8137582",
-"interface_leap_1_1_unity_1_1_interaction_1_1_i_internal_interaction_controller.html#a1400c6c5cfdc47c9051bb995b83dfbe8",
-"struct_leap_1_1_unity_1_1_animation_1_1_internal_1_1_transform_selector.html#a59ca52f64306b282d45ff0f38564926b",
-"struct_leap_internal_1_1_l_e_a_p___c_o_n_f_i_g___r_e_s_p_o_n_s_e___e_v_e_n_t.html#ad0da4218f271866fe990e4d3a8ab3770"
+"core.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
